c167b41082308c40cb850593326f63dc433bf47c
tlucch <thomasflucchetta@gmail.com>
2022-10-04 23:32:26 -0300

    add comments

--- API-info.colector.py
+++ API-info.colector.py
@@ -58,38 +58,49 @@ for summoner in summoner_list:
         for match in my_matches:
                 
                 try:
+                    #We get match basic info using match API
                     match_detail_end = watcher.match.by_id(region2, match)
                     
                     #We will only fecth info from classic games, not ARAMs
                     if match_detail_end["info"]["gameMode"] == "CLASSIC":
 
+                        #We get info minute by minute about the match using timeline API
                         match_detail = watcher.match.timeline_by_match(region2, match)
 
-                        #Create dict for team1 and team2
+                        #Create dict for team1 and team2 that will later become a df
                         team_1 = {"Gold": 0, "Level": 0, "Minions": 0, "Jungle_minions": 0, "Kills": 0, "Assists": 0, "Deaths": 0, "Plates": 0, "Towers": 0, "Dragons": 0, "Heralds": 0, "Sight_wards": 0, "Control_wards": 0}
                         team_2 = {"Gold": 0, "Level": 0, "Minions": 0, "Jungle_minions": 0, "Kills": 0, "Assists": 0, "Deaths": 0, "Plates": 0, "Towers": 0, "Dragons": 0, "Heralds": 0, "Sight_wards": 0, "Control_wards": 0}
                         
-                        #Iteration
+                        #Get All data available in minute 14 for team1
                         for i in range(1, 6): 
                            team_1["Gold"] = team_1["Gold"] + match_detail["info"]["frames"][14]["participantFrames"][f"{i}"]["totalGold"]
                            team_1["Level"] = team_1["Level"] + match_detail["info"]["frames"][14]["participantFrames"][f"{i}"]["level"]
                            team_1["Minions"] = team_1["Minions"] + match_detail["info"]["frames"][14]["participantFrames"][f"{i}"]["minionsKilled"]
                            team_1["Jungle_minions"] = team_1["Jungle_minions"] + match_detail["info"]["frames"][14]["participantFrames"][f"{i}"]["jungleMinionsKilled"]
-                           
+
+                        #Get All data available in minute 14 for team2   
                         for i in range(6, 11): 
                            team_2["Gold"] = team_2["Gold"] + match_detail["info"]["frames"][14]["participantFrames"][f"{i}"]["totalGold"]
                            team_2["Level"] = team_2["Level"] + match_detail["info"]["frames"][14]["participantFrames"][f"{i}"]["level"]
                            team_2["Minions"] = team_2["Minions"] + match_detail["info"]["frames"][14]["participantFrames"][f"{i}"]["minionsKilled"]
                            team_2["Jungle_minions"] = team_2["Jungle_minions"] + match_detail["info"]["frames"][14]["participantFrames"][f"{i}"]["jungleMinionsKilled"]
                            
-                           
+                        #Get mean level for each team  
                         team_1["Level"] = team_1["Level"]/5
-                        team_2["Level"] = team_2["Level"]/5        
+                        team_2["Level"] = team_2["Level"]/5
+
+                        #Get gold difference         
                         team_1["Gold_diff"] = team_1["Gold"] - team_2["Gold"]
                         team_2["Gold_diff"] = team_2["Gold"] - team_1["Gold"]
                         
+                        #The rest of the info is not available in the minute 14 data, so it has to be scarped minute by minute that why we iterate from 1 to 14
                         for i in range(1, 15):
+
+                            #For each minute a list of events its presented, so we can iterate through each event and get necessary info
                             for j in match_detail["info"]["frames"][i]["events"]:
+
+                                #Get Kills, deaths and assists. Each event has a KillerID. 
+                                #If the Killer ID is between 1 and 5 is corresponds to team1, if its bigger than 5 is for team2. This pattern is repeated through out the iteration of events
                                 if (j["type"] == "CHAMPION_KILL") and (1 <= j["killerId"] <= 5):
                                     team_1["Kills"] += 1
                                     team_2["Deaths"] += 1
@@ -105,6 +116,7 @@ for summoner in summoner_list:
                                     except:
                                         pass
                                 
+                                #Get Turret plates destroyed
                                 if (j["type"] == "TURRET_PLATE_DESTROYED") and (1 <= j["killerId"] <= 5):
                                     team_1["Plates"] += 1
                                 if (j["type"] == "TURRET_PLATE_DESTROYED") and (j["killerId"] > 5):
@@ -115,6 +127,7 @@ for summoner in summoner_list:
                                 if (j["type"] == "BUILDING_KILL") and (j["teamId"] == 100):
                                     team_2["Towers"] += 1 
                                 
+                                #Get Dragons and Heralds
                                 if (j["type"] == "ELITE_MONSTER_KILL") and (1 <= j["killerId"] <= 5):
                                     if j["monsterType"] == "DRAGON":
                                         team_1["Dragons"] += 1
@@ -127,6 +140,7 @@ for summoner in summoner_list:
                                     elif j["monsterType"] == "RIFTHERALD":
                                         team_2["Heralds"] += 1                
                                 
+                                #Get wards placed
                                 if (j["type"] == "WARD_PLACED" and j["wardType"] == "CONTROL_WARD") and (1 <= j["creatorId"] <= 5):
                                     team_1["Control_wards"] += 1
                                 if (j["type"] == "WARD_PLACED" and j["wardType"] == "CONTROL_WARD") and (j["creatorId"] > 5):
@@ -137,6 +151,7 @@ for summoner in summoner_list:
                                 if (j["type"] == "WARD_PLACED" and (j["wardType"] == "SIGHT_WARD" or j["wardType"] == "YELLOW_TRINKET")) and (j["creatorId"] > 5):
                                     team_2["Sight_wards"] += 1
                         
+                        #Get info of team win or lose
                         if match_detail_end["info"]["teams"][0]["win"] == True:
                             team_1["Win"] = 1
                             team_2["Win"] = 0
@@ -144,8 +159,11 @@ for summoner in summoner_list:
                             team_1["Win"] = 0
                             team_2["Win"] = 1
                         
+                        #Dicts of team1 and team2 to df
                         team_1 = pd.DataFrame([team_1], columns=team_1.keys())
                         team_2 = pd.DataFrame([team_2], columns=team_2.keys())
+
+                        #Concat both df to df_final
                         df_temp = pd.concat([team_1, team_2])
                         df_final = pd.concat([df_final, df_temp])
                         
@@ -156,5 +174,6 @@ for summoner in summoner_list:
         print(f"Summoner not found: {summoner}")
     
     print(f"{count}/100")
-        
+
+#Send df to an excel        
 df_final.to_excel(f"{region}-History")
